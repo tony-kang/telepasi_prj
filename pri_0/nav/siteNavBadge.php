67043e61787c2c2fe4f6640ccbacf264ffc2ecdb
Kang ByungWoo <kangbyungwoo@Kangui-MacBookPro.local>
2022-07-27 05:19:27 +0900

    배지 클래스화

--- pri_0/nav/siteNavBadge.php
+++ pri_0/nav/siteNavBadge.php
@@ -5,6 +5,9 @@
  * Description
  *
  */
+___debug(__FILE__.__LINE__);
+
+$myBadge = new MyBadge();
 
 $_m7Day = ___endDate('-7 day');
 //$_m7Day2 = ___dayAfter('-7 day');
@@ -15,39 +18,47 @@ $_m7Day = ___endDate('-7 day');
 
 $sql = db_getDbData(S_DB,'t_uploadFile','count(*) as cnt',sprintf('T.regDate >= "%s"' ,$_m7Day).' and docCode like "%WEEK%"');
 $cnt_1 = $sql['cnt'];
-if ($cnt_1) ___menuSubBadge($_nav['menuDocument'],'documentWeek',$cnt_1);
+//if ($cnt_1) ___menuSubBadge($_nav['menuDocument'],'documentWeek',$cnt_1);
+if ($cnt_1) $myBadge->newMenuBadge($_nav['menuDocument'],'documentWeek',$cnt_1)->make();
 
 $sql = db_getDbData(S_DB,'t_uploadFile','count(*) as cnt',sprintf('T.regDate >= "%s"' ,$_m7Day).' and docCode like "%MONTH%"');
 $cnt_2 = $sql['cnt'];
-if ($cnt_2) ___menuSubBadge($_nav['menuDocument'],'documentMonth',$cnt_2);
+//if ($cnt_2) ___menuSubBadge($_nav['menuDocument'],'documentMonth',$cnt_2);
+if ($cnt_2) $myBadge->newMenuBadge($_nav['menuDocument'],'documentMonth',$cnt_2)->make();
 
 $_badge['menuDocumentCnt'] = $cnt_1 + $cnt_2;
 $_badge['headerDocumentCnt'] = $_badge['menuDocumentCnt'];
-if ($_badge['menuDocumentCnt']) ___menuBadge($_nav['menuDocument'],$_badge['menuDocumentCnt']);
-
+//if ($_badge['menuDocumentCnt']) ___menuBadge($_nav['menuDocument'],$_badge['menuDocumentCnt']);
+if ($_badge['menuDocumentCnt']) $myBadge->newMenuBadge($_nav['menuDocument'],'',$_badge['menuDocumentCnt'])->make();
 
 /*************************************************************
  * 결재 개수 배지
  ************************************************************/
-if ($_site['feature']['approval']['viewBadge']) {
-    include_once MY_SRC_PATH . '/approval/inc_query.php';
-    $_badge['menuApprovalCnt'] = 0;
-    $_badge['headerApprovalCnt'] = 0;
-    foreach ($approvalBadgeQueryArr_badge as $qKey => $badgeQery) {
-        $approvalBadgeQuery = sprintf('deleted = 0 and aTempSave=%d', ($qKey == 'myStart.tempSave') ? 1 : 0);  //임시저장 처리
-        $approvalBadgeQuery .= $approvalBadgeQueryArr_badge[$qKey]['q'];
-        if ($badgeQery['submenu'] == 'myStart_3') $approvalBadgeQuery .= sprintf(' and T.regDate >= "%s"', $_m7Day);
+include_once MY_SRC_PATH . '/approval/inc_query.php';
+$_badge['menuApprovalCnt'] = 0;
+$_badge['headerApprovalCnt'] = 0;
+foreach ($approvalBadgeQueryArr_badge as $qKey => $badgeQery) {
+    $approvalBadgeQuery = sprintf('deleted = 0 and aTempSave=%d', ($qKey == 'myStart.tempSave') ? 1 : 0);  //임시저장 처리
+    $approvalBadgeQuery .= $approvalBadgeQueryArr_badge[$qKey]['q'];
+    if ($badgeQery['submenu'] == 'myStart_3') $approvalBadgeQuery .= sprintf(' and T.regDate >= "%s"', $_m7Day);
 
-        $sql = db_getDbData(S_DB, 't_approval', 'count(*) as cnt', $approvalBadgeQuery);
-        //___echo($sql['q']);
+    $sql = db_getDbData(S_DB, 't_approval', 'count(*) as cnt', $approvalBadgeQuery);
+    //___echo($sql['q']);
 
-        //2022.05.05 : 전자결재 특정 항목을 0으로 표시함
-        if ($_site['feature']['approval']['count'][$qKey.'.zero']) $sql['cnt'] = 0;
+    //2022.05.05 : 전자결재 특정 항목을 0으로 표시함
+    if ($_site['feature']['approval']['count'][$qKey.'.zero']) $sql['cnt'] = 0;
 
-        $cnt_1 = $sql['cnt'];
-        $_badge['menuApprovalCnt'] += $sql['cnt'];
-        $_badge['headerApprovalCnt'] += $sql['cnt'];
-        if ($cnt_1) ___menuSubBadge($_nav['menuApproval'], $badgeQery['submenu'], $cnt_1);
-    }
-    if ($_badge['menuApprovalCnt']) ___menuBadge($_nav['menuApproval'], $_badge['menuApprovalCnt']);
+    $cnt_1 = $sql['cnt'];
+    $_badge['menuApprovalCnt'] += $sql['cnt'];
+    $_badge['headerApprovalCnt'] += $sql['cnt'];
+    //if ($cnt_1) ___menuSubBadge($_nav['menuApproval'], $badgeQery['submenu'], $cnt_1);
+    if ($cnt_1) $myBadge->newMenuBadge($_nav['menuApproval'],$badgeQery['submenu'], $cnt_1)->make();
 }
+
+//if ($_badge['menuApprovalCnt']) ___menuBadge($_nav['menuApproval'], $_badge['menuApprovalCnt']);
+
+if (HAS_APPROVAL_MENU && APPROVAL_VIEW_BADGE && $_badge['menuApprovalCnt']) {
+    $myBadge->newMenuBadge($_nav['menuApproval'],'', $_badge['menuApprovalCnt'])->make();
+}
+
+$myBadge->print();
--- pri_0/nav/siteTopBadge.php
+++ pri_0/nav/siteTopBadge.php
@@ -6,21 +6,33 @@
  *
  */
 
-$iconBadgeArr = [];
-$hasBadge = false;
+___debug(__FILE__.__LINE__);
+//$iconBadgeArr = [];
+//$hasBadge = false;
+//
+//if ($_site['feature']['approval']['viewBadge'] && $_badge['headerApprovalCnt']) {
+//    $hasBadge = true;
+//    $badge_approval = ['icon' => 'fal fa-envelope-open', 'href' => '?cfg=menuApproval&mN=myStart_1&mT=myStart&mTs=confirm', 'data' => ['badgeText' => $_badge['headerApprovalCnt'], 'caption' => '결재']];
+//    array_push($iconBadgeArr, $badge_approval);
+//}
+//
+//if ($_badge['headerDocumentCnt']) {
+//    $hasBadge = true;
+//    $badge_doc = ['icon' => 'fal fa-album-collection', 'href' => '?cfg=menuDocument&mN=documentWeek&mT=report&mTs=week', 'data' => ['badgeText' => $_badge['headerDocumentCnt'], 'caption' => '문서']];
+//    array_push($iconBadgeArr, $badge_doc);
+//}
+//
+//if ($hasBadge) {
+//    echo ___iconBadge($iconBadgeArr);
+//}
 
-if ($_site['feature']['approval']['viewBadge'] && $_badge['headerApprovalCnt']) {
-    $hasBadge = true;
-    $badge_approval = ['icon' => 'fal fa-envelope-open', 'href' => '?cfg=menuApproval&mN=myStart_1&mT=myStart&mTs=confirm', 'data' => ['badgeText' => $_badge['headerApprovalCnt'], 'caption' => '결재']];
-    array_push($iconBadgeArr, $badge_approval);
+$myBadge = new MyBadge();
+if (HAS_APPROVAL_MENU && APPROVAL_VIEW_BADGE && $_badge['headerApprovalCnt']) {
+    $myBadge->newTopBadge($_badge['headerApprovalCnt'])->icon('fal fa-envelope-open')->href('?cfg=menuApproval&mN=myStart_1&mT=myStart&mTs=confirm')->caption('결재')->make();
 }
 
 if ($_badge['headerDocumentCnt']) {
-    $hasBadge = true;
-    $badge_doc = ['icon' => 'fal fa-album-collection', 'href' => '?cfg=menuDocument&mN=documentWeek&mT=report&mTs=week', 'data' => ['badgeText' => $_badge['headerDocumentCnt'], 'caption' => '문서']];
-    array_push($iconBadgeArr, $badge_doc);
+    $myBadge->newTopBadge($_badge['headerDocumentCnt'])->icon('fal fa-album-collection')->href('?cfg=menuDocument&mN=documentWeek&mT=report&mTs=week')->caption('문서')->make();
 }
 
-if ($hasBadge) {
-    echo ___iconBadge($iconBadgeArr);
-}
+$myBadge->print();
